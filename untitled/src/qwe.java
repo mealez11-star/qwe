cfc28d5636052f1bc9f01cf165ba32c21ff3f11b
qwe <mealez11@gmail.com>
2026-02-16 21:32:42 +0300

    hello

---
+++ untitled/src/qwe.java
@@ -0,0 +1,3 @@
+public class qwe {
+    int a = 5;
+}
---
+++ untitled2/src/qqq.java
@@ -0,0 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class qqq {
+    static void main() {
+        System.out.println("Hello");
+    }
+}
